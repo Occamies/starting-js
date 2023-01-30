cd0d42420eed1181e3deb287d41ae65ac6ecbc97
Lasse Skovdal Knudsen <lakn1@videndjurs.net>
2023-01-30 14:06:18 +0100

    first

---
+++ src/app.js
@@ -0,0 +1,15 @@
+// import Initialize from "./Initialize";
+import "../css/style.scss";
+import Initialize from "./Initialize";
+
+
+
+// **  IIFE: Immediately Invoked Function Expression  */
+
+
+
+(function () {
+
+  let init = new Initialize();
+
+})();
